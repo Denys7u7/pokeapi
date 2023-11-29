25199c92cc977dca5573884f991580d3df73b370
DenysC <denys_cruz@unicomer.com>
2023-11-28 22:51:06 -0600

    Application completed Last action, continued the configuration and use of state, angular material, swipper, virtual scroll and removed the requests from the components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { PokemonSelectionComponent } from './components/pokemon-selection/pokemon-selection.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ConfigurationComponent },
+  { path: 'loader/:nextComponent/:message', component: LoaderComponent },
+  { path: 'loader', component: LoaderComponent },
+  { path: 'selection', component: PokemonSelectionComponent },
+  { path: 'profile', component: MyProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
